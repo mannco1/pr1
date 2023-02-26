6ea31a710ee9fb9708110c5d70cc3ad5280ff055
mannco1 <70008207+mannco1@users.noreply.github.com>
2023-02-26 17:33:31 +0300

    Add files via upload

---
+++ pr2/2-zadania/circlemain.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include "Hcircler.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	circle A(0, 0, 0);
+	circle B(0, 0, 0);
+	circle C(0, 0, 0);
+
+	float j1, j2, j3,a1,a2,a3;
+
+
+	cout << "x, y и радиус окружности A\n";
+	cin >> j1 >> j2 >> j3;
+	A.set_circle(j1, j2, j3);
+
+	cout << "x, y и радиус окружности B\n";
+	cin >> j1 >> j2 >> j3;
+	B.set_circle(j1, j2, j3);
+
+	cout << "x, y и радиус окружности C\n";
+	cin >> j1 >> j2 >> j3;
+	C.set_circle(j1, j2, j3);
+
+	cout << "\n";
+
+	cout << "Площадь окружности A - " << A.square() << "\n";
+	cout << "Площадь окружности B - " << B.square() << "\n";
+	cout << "Площадь окружности C - " << C.square() << "\n";
+
+	
+
+	float a, b, c;
+	float  r, x_cntr, y_cntr;
+
+	cout << "Введите треугольник abc\n";
+	cin >> a >> b >> c;
+
+	if (A.triangle_around(a, b, c)) cout << "Окружность A можно  описать в треугольнике\n";
+	else cout << "Окружность A нельзя вписать треугольник\n";
+
+	if (B.triangle_around(a, b, c)) cout << "Окружность B можно  описать в треугольнике\n";
+	else cout << "Окружность B нельзя вписать треугольник\n";
+
+	if (C.triangle_around(a, b, c)) cout << "Окружность C можно  описать в треугольнике\n";
+	else cout << "Окружность C нельзя вписать треугольник\n";
+
+	if (A.triangle_in(a, b, c)) cout << "Окружность A можно вписать в   треугольник\n";
+	else cout << "Окружность A нельзя вписать треугольник\n";
+
+	if (B.triangle_in(a, b, c)) cout << "Окружность B можно вписать в   треугольник\n";
+	else cout << "Окружность B нельзя вписать треугольник\n";
+
+	if (C.triangle_in(a, b, c)) cout << "Окружность C можно вписать в   треугольник\n";
+	else cout << "Окружность C нельзя вписать треугольник\n";
+	cout << "\n";
+	cout << "\n";
+
+	cout << "Проверка пересечения окружностей - радиус,центры по X и Y \n";
+	cin >>a1 >> a2 >> a3;
+	if (A.check_circle(a1, a2, a3)) cout << "окружность A пересекается с данной окружностью \n";
+	else cout << "A непересекается\n";
+
+	if (B.check_circle(a1, a2, a3)) cout << "окружность B пересекается с данной окружностью\n";
+	else cout << "B непересекается\n";
+
+	if (C.check_circle(a1, a2, a3)) cout << "окружность C пересекается с данной окружностью\n";
+	else cout << "C непересекается\n";
+
+
+	return 0;
+}
---
+++ pr2/3-zadanie/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include "Hfigure.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float X1, X2, X3, X4, Y1, Y2, Y3, Y4; 
+	figure A(0,0,0,0,0,0,0,0);
+	figure B(0, 0, 0, 0, 0, 0, 0, 0);
+	figure C(0, 0, 0, 0, 0, 0, 0, 0);
+
+	cout << " A (x 1-4),(y 1-4)\n";
+	cin >> X1>> X2 >>X3 >>X4>> Y1>>  Y2>> Y3 >> Y4;
+	A.set_fig(X1, X2, X3, X4, Y1 , Y2 , Y3, Y4);
+
+	cout << " B (x 1-4),(y 1-4)\n";;
+	cin >> X1 >> X2 >> X3 >> X4 >> Y1 >> Y2 >> Y3 >> Y4;
+	B.set_fig(X1, X2, X3, X4, Y1, Y2, Y3, Y4);
+
+	cout << " C (x 1-4),(y 1-4)\n";
+	cin >> X1 >> X2 >> X3 >> X4 >> Y1 >> Y2 >> Y3 >> Y4;
+	C.set_fig(X1, X2, X3, X4, Y1, Y2, Y3, Y4);
+
+	cout << "A: "; A.show();
+	cout << "B: "; B.show();
+	cout << "C: "; C.show();
+
+	cout << "\n";
+
+	if (A.is_prug()) cout << "A\n";
+	if (B.is_prug()) cout << "A\n";
+	if (C.is_prug()) cout << "A\n";
+
+	cout << "\n";
+
+	if (A.is_square()) cout << " A \n";
+	if (B.is_square()) cout << " B \n";
+	if (C.is_square()) cout << " C \n";
+
+	cout << "\n";
+
+	if (A.is_romb()) cout << " A \n";
+	if (B.is_romb()) cout << " B \n";
+	if (C.is_romb()) cout << " C \n";
+
+	cout << "\n";
+
+	if (A.is_in_circle()) cout << "A\n";
+	if (B.is_in_circle()) cout << "B\n";
+	if (C.is_in_circle()) cout << "C\n";
+
+	cout << "\n";
+
+	if (A.is_out_circle()) cout << "A\n";
+	if (B.is_out_circle()) cout << "A\n";
+	if (C.is_out_circle()) cout << "C\n";
+}
